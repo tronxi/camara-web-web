f25a3b4acbe9a0a82f4c9ad254af3810aef9c90e
tronxi <tronxi97@gmail.com>
2018-07-30 21:32:21 +0200

    cambio src

--- javascprit/actualizarImagen.js
+++ javascprit/actualizarImagen.js
@@ -10,8 +10,9 @@ var actualizar = setInterval(function ()
 function actualizarImagen()
 {
     var aleatorio = Math.ceil(Math.random()*100);
-    contenedor = document.getElementById("contenedorImagen");
+    contenedor = document.getElementById("imagen");
     var src = "php/imagen_camara.png?'"+aleatorio+"'";
-    contenedor.innerHTML = "<img src='"+src+"' class='img-thumbnail' alt='error imagen' id='imagen'>";
+    imagen.src = src;
+    //contenedor.innerHTML = "<img src='"+src+"' class='img-thumbnail' alt='error imagen' id='imagen'>";
     console.log(src);
 }
