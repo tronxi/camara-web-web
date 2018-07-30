f9d199e427004e423353f1dada18f7a21937dc3a
tronxi <tronxi97@gmail.com>
2018-07-30 21:25:30 +0200

    mostrar src

--- javascprit/actualizarImagen.js
+++ javascprit/actualizarImagen.js
@@ -5,12 +5,13 @@ function inicializarEventos()
 var actualizar = setInterval(function ()
 {
     actualizarImagen()
-    console.log("actualizando");
 }, 100);
 
 function actualizarImagen()
 {
     var aleatorio = Math.ceil(Math.random()*100);
     contenedor = document.getElementById("contenedorImagen");
-    contenedor.innerHTML = "<img src='php/imagen_camara.png?'+aleatorio+'' class='img-thumbnail' alt='error imagen' id='imagen'>";
+    var src = "php/imagen_camara.png?'+aleatorio+'";
+    contenedor.innerHTML = "<img src='"+src+"' class='img-thumbnail' alt='error imagen' id='imagen'>";
+    console.log(src);
 }
