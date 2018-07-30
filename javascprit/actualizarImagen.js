2e1b9a7c3adaab2d6496b53eb53b1188c6d4bdf0
tronxi <tronxi97@gmail.com>
2018-07-30 21:36:56 +0200

    prueba sin aleatorio

--- javascprit/actualizarImagen.js
+++ javascprit/actualizarImagen.js
@@ -11,7 +11,8 @@ function actualizarImagen()
 {
     var aleatorio = Math.ceil(Math.random()*100);
     contenedor = document.getElementById("imagen");
-    var src = "php/imagen_camara.png?'"+aleatorio+"'";
+    //var src = "php/imagen_camara.png?'"+aleatorio+"'";
+    var src = "php/imagen_camara.png";
     imagen.src = src;
     //contenedor.innerHTML = "<img src='"+src+"' class='img-thumbnail' alt='error imagen' id='imagen'>";
     console.log(src);
