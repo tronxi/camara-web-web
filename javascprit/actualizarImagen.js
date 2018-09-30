31054c5c1f018c522c28829a43ba7088b25c372d
Sergio Garcia Sanchez <sergio.gsanchez@alumnos.upm.es>
2018-10-01 00:16:19 +0200

    cambio resolucion

--- javascprit/actualizarImagen.js
+++ javascprit/actualizarImagen.js
@@ -13,7 +13,7 @@ function actualizarImagen()
     contenedor = document.getElementById("imagen");
     var src = "php/imagen_camara.png?'"+aleatorio+"'";
     imagen.src = src;
-    imagen.width = 640;
-    imagen.height = 480;
+    imagen.width = screen.width;
+    //imagen.height = 480;
     console.log(src);
 }
